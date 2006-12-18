2e00353c71bb919d3ccebec213a354f417ffe210
sourcewalker <sourcewalker@5758883d-db1f-0410-8b85-897ae89228ee>
2006-12-18 17:03:27 +0000

    REST API version is implemented.

--- branches/webos-goodies_update/delicious.rb
+++ branches/webos-goodies_update/delicious.rb
@@ -1,5 +1,7 @@
 # del.icio.us API class
 
+require 'time'
+require 'rexml/document'
 require 'webapi/rest'
 require 'webapi/json'
 
@@ -7,24 +9,50 @@ module WebAPI
 
   class Delicious
 
+    module Util
+      private
+      def str(s)
+        s ? s.to_s : ''
+      end
+      def array(a)
+        case a
+        when String
+          a.split(' ')
+        when NilClass
+          []
+        else
+          a.to_a
+        end
+      end
+      def integer(n)
+        n ? n.to_i : 0
+      end
+    end
+
     class Post
+      include Util
       def initialize(src = nil)
         set(src)
       end
       def set(src)
         case src
         when Hash
-          @title = src['d'].to_s
-          @url   = src['u'].to_s
-          @note  = src['n'].to_s
-          @tags  = src['t'].to_a
+          @title = str(src['d'])
+          @url   = str(src['u'])
+          @note  = str(src['n'])
+          @tags  = array(src['t'])
           @date  = nil
-        when NilClass, FalseClass
-          @title = nil
-          @url   = nil
-          @note  = nil
-          @tags  = nil
+        when REXML::Element
+          @title = str(src.attributes['description'])
+          @url   = str(src.attributes['href'])
+          @note  = str(src.attributes['extended'])
+          @tags  = array(src.attributes['tag'])
           @date  = nil
+          if src.attributes['time']
+            @date = Time.iso8601(src.attributes['time'])
+          end
+        when NilClass
+          @title = @url = @note = @tags = @date  = nil
         else
           raise
         end
@@ -33,19 +61,23 @@ module WebAPI
     end
 
     class Tag
+      include Util
       def initialize(arg1 = nil, arg2 = nil)
         set(arg1, arg2)
       end
       def set(arg1 = nil, arg2 = nil)
         if arg2
-          @name  = arg1.to_s
-          @count = arg2.to_i
+          @name  = str(arg1)
+          @count = integer(arg2)
         else
           case arg1
           when Array
-            @name  = arg1[0].to_s
-            @count = arg2[1].to_i
-          when NilClass, FalseClass
+            @name  = str(arg1[0])
+            @count = integer(arg2[1])
+          when REXML::Element
+            @name  = str(arg1.attributes['tag'])
+            @count = integer(arg1.attributes['count'])
+          when NilClass
             @name  = ''
             @count = 0
           else
@@ -56,47 +88,94 @@ module WebAPI
       attr_accessor :name, :count
     end
 
-    def url_escape(src)
-      src
+    class LimiterBase
+      def execute()
+        yield
+      end
     end
 
-    def initialize(username, password = nil)
-      if(password)
+    class ApiLimitter < LimiterBase
+      def initialize(interval = 1.5)
+        @interval = interval
+        @release_time = Time.now
+      end
+      def execute()
+        now = Time.now
+        sleep(@release_time - now) if now < @release_time
+        result = super
+        @release_time = Time.now + @interval
+        result
+      end
+    end
+
+    DefaultLimiter = ApiLimitter.new
+    NullLimiter    = LimiterBase.new
+
+    def initialize(username, password = nil, limiter = nil)
+      @limiter  = limiter
+      @username = username.to_s
+      if password
         protocol  = REST.https('api.del.icio.us')
         auth      = REST.basic_auth(username, password);
+        @api_mode = true
+        @limiter  = DefaultLimiter unless @limiter
       else
         protocol  = REST.http('del.icio.us')
         auth      = nil
+        @api_mode = false
+        @limiter  = NullLimiter unless @limiter
       end
-      @username = username.to_s
       @rest     = REST.new(protocol, auth)
     end
 
     def get_posts(opt = {})
-      get_posts_json(opt)
+      @api_mode ? get_posts_api(opt) : get_posts_json(opt)
     end
 
     def get_tags()
-      get_tags_json()
+      @api_mode ? get_tags_api() : get_tags_json()
+    end
+
+    private #---------------------------------------------------------
+
+    def http_get(url, params = {})
+      @limiter.execute do @rest.get(url, params) end
+    end
+
+    def get_posts_api(opt = {})
+      params = {}
+      params['tag'] = opt['tags'].join(' ') if opt['tags']
+      doc = REXML::Document.new(http_get('/v1/posts/all', params))
+      posts = []
+      doc.elements.each('posts/post') do |element|
+        posts << Post.new(element)
+      end
+      posts
     end
 
     def get_posts_json(opt)
-      tags = opt['tags'] ? opt['tags'] : []
+      tags = opt['tags'] ? '/' + opt['tags'].join(' ') : ''
       count = opt['count'] ? opt['count'] : 100
-      url = '/feeds/json/' + @rest.urlencode(@username + '/' + tags.join(' '))
+      url = '/feeds/json/' + @rest.urlencode(@username + tags)
       params = { 'count' => count, 'raw' => '' }
-      response = @rest.get(url, params)
-      json = Json.new.parse(response)
-      json.map! do |post|
+      Json.new.parse(http_get(url, params)).map! do |post|
         Post.new(post)
       end
+    end
+
+    def get_tags_api()
+      doc = REXML::Document.new(http_get('/v1/tags/get'))
+      tags = []
+      doc.elements.each('tags/tag') do |element|
+        tags << Tag.new(element)
+      end
+      tags
     end
 
     def get_tags_json()
       url = '/feeds/json/tags/' + @rest.urlencode(@username)
       params = { 'atleast' => 1, 'raw' => '' }
-      response = @rest.get(url, params)
-      json = Json.new.parse(response)
+      json = Json.new.parse(http_get(url, params))
       result = []
       json.each do |key, value|
         result << Tag.new(key, value)
