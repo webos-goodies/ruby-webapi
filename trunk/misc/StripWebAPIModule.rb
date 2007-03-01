5ceb30c3c0846fb6c860017ee8e37901fe1facde
sourcewalker <sourcewalker@5758883d-db1f-0410-8b85-897ae89228ee>
2007-03-01 15:33:22 +0000

    Added comment strip mode to StripWebAPIModule.rb.

--- trunk/misc/StripWebAPIModule.rb
+++ trunk/misc/StripWebAPIModule.rb
@@ -1,10 +1,14 @@
 #! /usr/bin/ruby
 
+remove_comment = false
+
 lines = $<.readlines
 
 # remove WebAPI module and adjust indent.
 lines.map! do |line|
   if /(^module WebAPI)|(^end)/ === line
+    nil
+  elsif remove_comment && /^\s*\#/ === line
     nil
   elsif /^  (.*)/m === line
     $1
