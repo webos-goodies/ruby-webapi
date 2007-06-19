9d912e702743456efe4d7e40c55749537400d93e
sourcewalker <sourcewalker@5758883d-db1f-0410-8b85-897ae89228ee>
2007-06-19 16:37:17 +0000

    delicious.rb was rewritten, unit test for delicious.rb was added and rdoc documents are added.

--- test/test.rb
+++ test/test.rb
@@ -1,4 +1,13 @@
 #! /usr/bin/ruby
 
+$: << File.dirname(__FILE__) + '/..'
+
+begin
+require 'rubygems'
+rescue LoadError
+end
+
 require 'test/unit'
+require 'flexmock'
+require 'test_delicious.rb'
 require 'test_json.rb'
