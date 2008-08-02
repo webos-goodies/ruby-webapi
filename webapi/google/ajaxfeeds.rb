dcbe935cd51f76891a728ad613369c04d244f994
sourcewalker <sourcewalker@5758883d-db1f-0410-8b85-897ae89228ee>
2008-08-02 14:22:30 +0000

    Fixed bugs in WebAPI::Google::AjaxFeeds.

--- webapi/google/ajaxfeeds.rb
+++ webapi/google/ajaxfeeds.rb
@@ -26,7 +26,7 @@ module WebAPI
           def content()         @content        ||(@content        =(@data['content']       ||'').to_s) end
           def content_snippet() @content_snippet||(@content_snippet=(@data['contentSnippet']||'').to_s) end
           def categories()      @categories     ||(@categories     = @data['categories']    ||[])       end
-          attr_reader :title, :url
+          attr_reader :title, :link
         end
 
         include Enumerable
@@ -39,25 +39,30 @@ module WebAPI
           @description      = ''
           @author           = ''
           @entries          = []
-          feed              = data['feed']
-          if @response_status == 200 && Hash === feed
-            @title       = (feed['title']      ||'').to_s
-            @link        = (feed['link']       ||'').to_s
-            @description = (feed['description']||'').to_s
-            @author      = (feed['author']     ||'').to_s
-            entries      = feed['entries']
-            entries.each{|e| @entries << FeedEntry.new(e) } if Array === entries
+          response_data     = data['responseData']
+          if @response_status == 200 && Hash === response_data
+            feed = response_data['feed']
+            if Hash === feed
+              @title       = (feed['title']      ||'').to_s
+              @link        = (feed['link']       ||'').to_s
+              @description = (feed['description']||'').to_s
+              @author      = (feed['author']     ||'').to_s
+              entries      = feed['entries']
+              entries.each{|e|
+                @entries << FeedEntry.new(e)
+              } if Array === entries
+            end
           end
         end
-        def each(&block) if @entries then @entries.each(block) end self end
+        def each(&block) if @entries then @entries.each(&block) end ; self end
         def valid?() @response_status==200 end
         def empty?() @entries.empty? end
       end
 
       def self.load(url, params = {})
         raise ArgumentError.new('The second argument must be a hash.') if params && !(Hash === params)
-        params   = { q => url, v="1.0" }.update(params||{})
-        json     = Rest.http('ajax.googleapis.com').get('/ajax/services/feed/load', params)
+        params   = { 'q' => url, 'v' => '1.0' }.update(params||{})
+        json     = REST.new(REST.http('ajax.googleapis.com')).get('/ajax/services/feed/load', params)
         LoadResponse.new(JsonParser.new.parse(json))
       end
 
